fcd9efbb2aa9f3c1db2159762fa4269d521773db
build-failure <michael@lewkowski.de>
2019-12-05 18:24:59 +0100

    Add host network test

--- filter_plugins/vswitch_list.py
+++ filter_plugins/vswitch_list.py
@@ -6,7 +6,7 @@ def vswitch_list(host_vars, hosts, attr='hetzner_vswitch_host'):
     for host in hosts:
         host_config = host_vars.get(host)
         vswitch_configs = host_config.get(attr)
-        server_ip = host_config.get('ansible_host')
+        server_ip = get_default_ip_address(host_config)
         if vswitch_configs:
             verify_is_list(vswitch_configs, 'hetzner_vswitch_host variable must be a list.')
             for vswitch_config in vswitch_configs:
@@ -27,6 +27,13 @@ def vswitch_list(host_vars, hosts, attr='hetzner_vswitch_host'):
 
     return list(results.values())
 
+
+def get_default_ip_address(host_config):
+    ansible_host = host_config.get('ansible_host')
+    if ansible_host:
+        return ansible_host
+
+    return host_config.get('ansible_default_ipv4').get('address')
 
 def verify_is_list(value, msg):
     if not isinstance(value, list):
